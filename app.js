552054747417334029a78f1e7ca96bd6456f132c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-01-29 04:13:18 +0000

    linked to twitchAPI, now need to work woth data

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // run our JQUERY
+$(document).ready(function(){
+    // FREE CODE CAMP STREAM INFO AND STATUS API CALL
+     var url= "https://wind-bow.glitch.me/twitch-api/streams/freecodecamp";
+     $.getJSON(url,function(data1){
+         if(data1.stream === null) {
+             $("#fccStatus").html("Free Code Camp is currently OFFLINE!");
+         }else{
+            $("#fccStatus").html("Free Code Camp is currently ONLINE");
+         }
+    });
+    var followerURL = "https://wind-bow.glitch.me/twitch-api/users/freecodecamp/" 
+    
+})
